58548204d7f0467b253cba59803eac1ff8a58491
Luca Minudel <>
2025-08-16 20:37:13 +0100

    Beta: ApiGateway. updated url in the environment config file with the live API url, updated the deploy shell command

--- Beta/scripts/Deploy_ApiGatewayLambdaFunctions.sh
+++ Beta/scripts/Deploy_ApiGatewayLambdaFunctions.sh
@@ -6,7 +6,11 @@ dotnet publish WiseWords.ConversationsAndPosts.AWS.Lambdas.ApiGateway.csproj  -c
 cd publish && zip -r ../publish.zip . && cd ..
 rm -rf publish
 
-# Add --guided for 1st deploy
-sam deploy  --guided --template-file template_aws_prod.yaml --profile deploy-to-production-profile --capabilities CAPABILITY_IAM
+# Clean up any failed deployments
+aws cloudformation delete-stack --stack-name wisewords --region eu-west-2 --profile deploy-to-production-profile 2>/dev/null || true
+aws cloudformation wait stack-delete-complete --stack-name wisewords --region eu-west-2 --profile deploy-to-production-profile 2>/dev/null || true
 
-sam deploy --template-file template_aws_prod.yaml --stack-name wisewords --capabilities CAPABILITY_IAM
+# Deploy 
+# add -- guided for the 1st deploy
+# all command parameters will are in samconfig.toml (or will be saved there at the 1st execution)
+sam deploy --template-file template_aws_prod.yaml
